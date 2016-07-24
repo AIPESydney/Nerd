3dcd45295cee1330e72349f5cdd5cac7615662b6
Burak <burakiskender@hotmail.com>
2016-07-24 21:17:04 +1000

    account controller , ninject configuration added.

---
+++ Nerd.Api/Controllers/AccountController.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Nerd.Api;
+using Nerd.Api.Models;
+using System.Web.Http.Cors;
+
+namespace Nerd.Api.Controllers
+{
+
+    [Authorize]
+    [RoutePrefix("api/Account")]
+    //[RequireHttpsAttribute]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+
+    public class AccountController : ApiController
+    {
+        private ApplicationUserManager _userManager;
+        private const string LocalLoginProvider = "Local";
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                if (_userManager == null)
+                {
+                    _userManager = _userManager ?? Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                }
+                return _userManager ?? (_userManager = ApplicationUserManager.Create(
+                    new IdentityFactoryOptions<ApplicationUserManager>()
+                    {
+                        Provider = new IdentityFactoryProvider<ApplicationUserManager>()
+                    }, Request.GetOwinContext()));
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        private IAuthenticationManager AuthenticationManager
+        {
+            get { return Request.GetOwinContext().Authentication; }
+        }
+        public ISecureDataFormat<AuthenticationTicket> AccessTokenFormat { get; private set; }
+
+
+        public AccountController(ApplicationUserManager userManager,
+            ISecureDataFormat<AuthenticationTicket> accessTokenFormat)
+        {
+            UserManager = userManager;
+            AccessTokenFormat = accessTokenFormat;
+        }
+
+
+
+        // POST api/Account/Logout
+        [Route("Logout")]
+        public IHttpActionResult Logout()
+        {
+            AuthenticationManager.SignOut(CookieAuthenticationDefaults.AuthenticationType);
+            return Ok();
+        }
+
+
+
+        // POST api/Account/ChangePassword
+        [Route("ChangePassword")]
+        public async Task<IHttpActionResult> ChangePassword(ChangePasswordBindingModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            IdentityResult result = await UserManager.ChangePasswordAsync(User.Identity.GetUserId(), model.OldPassword,
+                model.NewPassword);
+
+            if (!result.Succeeded)
+            {
+                return GetErrorResult(result);
+            }
+
+            return Ok();
+        }
+
+        // POST api/Account/SetPassword
+        [Route("SetPassword")]
+        public async Task<IHttpActionResult> SetPassword(SetPasswordBindingModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            IdentityResult result = await UserManager.AddPasswordAsync(User.Identity.GetUserId(), model.NewPassword);
+
+            if (!result.Succeeded)
+            {
+                return GetErrorResult(result);
+            }
+
+            return Ok();
+        }
+
+
+
+        // POST api/Account/Register
+        [AllowAnonymous]
+        [Route("Register")]
+        public async Task<IHttpActionResult> Register(RegisterBindingModel model)
+        {
+            if (!ModelState.IsValid || model == null || string.IsNullOrEmpty(model.Email))
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = new ApplicationUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+            };
+
+            IdentityResult result = await UserManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded)
+            {
+                return GetErrorResult(result);
+            }
+
+            await UserManager.AddToRoleAsync(user.Id, "Users");
+
+
+            //create user in users table as well
+            //var userdata = new Data.User
+            //{
+            //    Created = DateTime.Now,
+            //    UserId = user.Id,
+            //    Deleted = false,
+            //    IsActive = true
+            //};
+
+            //await _unitofWork.UserRepository.InsertAsync(userdata);
+            return Ok();
+        }
+
+
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+
+                if (_userManager != null)
+                {
+                    _userManager.Dispose();
+                }
+
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Helpers
+
+        private IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+            {
+                return InternalServerError();
+            }
+
+            if (!result.Succeeded)
+            {
+                if (result.Errors != null)
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    // No ModelState errors are available to send, so just return an empty BadRequest.
+                    return BadRequest();
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            return null;
+        }
+
+        private static class RandomOAuthStateGenerator
+        {
+            private static RandomNumberGenerator _random = new RNGCryptoServiceProvider();
+
+            public static string Generate(int strengthInBits)
+            {
+                const int bitsPerByte = 8;
+
+                if (strengthInBits % bitsPerByte != 0)
+                {
+                    throw new ArgumentException("strengthInBits must be evenly divisible by 8.", "strengthInBits");
+                }
+
+                int strengthInBytes = strengthInBits / bitsPerByte;
+
+                byte[] data = new byte[strengthInBytes];
+                _random.GetBytes(data);
+                return HttpServerUtility.UrlTokenEncode(data);
+            }
+        }
+        #endregion
+
+
+
+    }
+}
