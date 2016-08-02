c3506c85e633fb76ed94c05d9fd5cd558f762538
Burak <burakiskender@hotmail.com>
2016-08-02 21:19:21 +1000

    twilio sms integration started

--- Nerd.Api/App_Start/IdentityConfig.cs
+++ Nerd.Api/App_Start/IdentityConfig.cs
@@ -3,12 +3,42 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Nerd.Api.Models;
+using System.Threading.Tasks;
+using Twilio;
+using System;
+using System.Diagnostics;
 
 namespace Nerd.Api
 {
     // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity 
     // and is used by the application.
+    public class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            throw new NotImplementedException();
+        }
+    }
 
+    public class SmsService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            //Twilio Begin
+            var twilio = new TwilioRestClient(
+              System.Configuration.ConfigurationManager.AppSettings["SMSAccountIdentification"],
+              System.Configuration.ConfigurationManager.AppSettings["SMSAccountPassword"]);
+
+            var result = twilio.SendMessage(System.Configuration.ConfigurationManager.AppSettings["SMSAccountFrom"], message.Destination, message.Body);
+
+            //Status is one of Queued, Sending, Sent, Failed or null if the number is not valid
+            Trace.TraceInformation(result.Status);
+            //Twilio doesn't currently have an async API, so return success.
+            return Task.FromResult(0);
+            //Twilio End
+
+        }
+    }
     public class ApplicationUserManager : UserManager<ApplicationUser>
     {
         public ApplicationUserManager(IUserStore<ApplicationUser> store)
